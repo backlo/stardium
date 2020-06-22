c641af11fd2531c235d0eeafc03eeb0e0a7d95b7
backlo <hjun4512@gmail.com>
2020-06-22 19:44:14 +0900

    refactor: AppWebConfigurer 어노테이션 구현

--- resource-api/src/main/java/com/bb/stardium/resource/config/AppWebConfigurer.java
+++ resource-api/src/main/java/com/bb/stardium/resource/config/AppWebConfigurer.java
@@ -3,11 +3,15 @@ package com.bb.stardium.resource.config;
 import com.bb.stardium.resolver.AuthorizePlayerArgumentResolver;
 import com.bb.stardium.service.player.PlayerService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@EnableWebMvc
+@Configuration
 public class AppWebConfigurer implements WebMvcConfigurer {
 
     private final PlayerService playerService;
